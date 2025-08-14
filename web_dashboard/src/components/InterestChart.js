3788c00eedd809bb832e06018002ff9a29312d16
Umaa-6183 <58160604+Umaa-6183@users.noreply.github.com>
2025-08-15 01:32:44 +0530

    Update InterestChart.js

--- web_dashboard/src/components/InterestChart.js
+++ web_dashboard/src/components/InterestChart.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import {
   Chart as ChartJS, CategoryScale, LinearScale,
@@ -7,25 +7,13 @@ import {
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const InterestChart = ({ feedbacks }) => {
-  const [interestCounts, setInterestCounts] = useState({});
-
-  useEffect(() => {
-    const counts = {};
-    feedbacks.forEach(fb => {
-      fb.future_interest.forEach(item => {
-        counts[item] = (counts[item] || 0) + 1;
-      });
-    });
-    setInterestCounts(counts);
-  }, [feedbacks]);
-
-  const data = {
-    labels: Object.keys(interestCounts),
+const InterestChart = ({ data }) => {
+  const chartData = {
+    labels: Object.keys(data),
     datasets: [
       {
         label: 'Interest Frequency',
-        data: Object.values(interestCounts),
+        data: Object.values(data),
         backgroundColor: 'rgba(75, 192, 192, 0.7)',
       }
     ]
@@ -34,7 +22,7 @@ const InterestChart = ({ feedbacks }) => {
   return (
     <div className="chart-card">
       <h3>📊 Interest Categories</h3>
-      <Bar data={data} />
+      <Bar data={chartData} />
     </div>
   );
 };
